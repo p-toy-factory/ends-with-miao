a45d53c2c1eb60d85613ab16ba345be555099cd7
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2023-06-13 18:38:15 +0800

    refactor: match Japanese syllabaries with general categories

--- packages/ends-with-miao/src/index.ts
+++ packages/ends-with-miao/src/index.ts
@@ -1,13 +1,22 @@
-function checkIfIncludesJapaneseSyllabaries(input: string): boolean {
-  // http://www.unicode.org/charts/nameslist/c_3040.html
-  // https://unicode.org/charts/nameslist/c_30A0.html
-  return /[\u3041-\u30FF]/.test(input);
+function includesHiragana(input: string) {
+  // https://unicode.org/iso15924/iso15924-codes.html#:~:text=2004%2D05%2D01-,Hira,-410
+  return /\p{Script=Hira}/gu.test(input);
+}
+
+function includesKatakana(input: string) {
+  // https://unicode.org/iso15924/iso15924-codes.html#:~:text=2007%2D07%2D02-,Kana,-411
+  return /\p{Script=Kana}/gu.test(input);
+}
+
+function includesJapaneseSyllabaries(input: string): boolean {
+  return includesHiragana(input) || includesKatakana(input);
 }
 
 export function endsWithMiao(input: string): string {
-  if (checkIfIncludesJapaneseSyllabaries(input)) {
+  if (includesJapaneseSyllabaries(input)) {
     return input;
   }
-  // https://www.zhangxinxu.com/study/201611/chinese-language-unicode-range.html#:~:text=20902%E5%AD%97-,4E00%2D9FA5,-%E5%9F%BA%E6%9C%AC%E6%B1%89%E5%AD%97%E8%A1%A5%E5%85%85
-  return input.replace(/(?:(?!喵)([\u4E00-\u9FA5]))([！。…!.])/g, "$1喵$2");
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Unicode_character_class_escape#scripts_and_script_extensions
+  // https://unicode.org/iso15924/iso15924-codes.html#:~:text=500-,Han,-(Hanzi%2C%20Kanji%2C%20Hanja
+  return input.replace(/(?:(?!喵)(\p{Script=Han}))([！。…!.])/gu, "$1喵$2");
 }
