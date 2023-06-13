c3f905040d1f1ac646bb895b9e4eca6d0814b91e
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2023-06-13 18:38:15 +0800

    refactor: match Japanese syllabaries with `Script` Unicode properties

--- packages/ends-with-miao/src/index.ts
+++ packages/ends-with-miao/src/index.ts
@@ -1,13 +1,16 @@
-function checkIfIncludesJapaneseSyllabaries(input: string): boolean {
-	// http://www.unicode.org/charts/nameslist/c_3040.html
-	// https://unicode.org/charts/nameslist/c_30A0.html
-	return /[\u3041-\u30FF]/.test(input);
+// About the `\p{sc=}` in the regex:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Unicode_character_class_escape#scripts_and_script_extensions
+// https://unicode.org/Public/UCD/latest/ucd/PropertyValueAliases.txt
+
+function includesJapaneseSyllabaries(input: string): boolean {
+	// See the comment on the top of the file.
+	return /\p{sc=Hira}|\p{sc=Kana}/gu.test(input);
 }
 
 export function endsWithMiao(input: string): string {
-	if (checkIfIncludesJapaneseSyllabaries(input)) {
+	if (includesJapaneseSyllabaries(input)) {
 		return input;
 	}
-	// https://www.zhangxinxu.com/study/201611/chinese-language-unicode-range.html#:~:text=20902%E5%AD%97-,4E00%2D9FA5,-%E5%9F%BA%E6%9C%AC%E6%B1%89%E5%AD%97%E8%A1%A5%E5%85%85
-	return input.replace(/(?:(?!喵)([\u4E00-\u9FA5]))([！。…!.])/g, "$1喵$2");
+	// See the comment on the top of the file.
+	return input.replace(/(?:(?!喵)(\p{sc=Han}))([！。…!.])/gu, "$1喵$2");
 }
