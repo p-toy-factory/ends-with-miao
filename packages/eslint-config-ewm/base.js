e169bedec999e66062f2cdaf64ae7fdaed19019a
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2023-06-24 15:51:26 +0800

    chore: eslint

---
+++ packages/eslint-config-ewm/base.js
@@ -0,0 +1,12 @@
+// @ts-check
+const js = require("@eslint/js");
+
+/** @type {import("eslint").Linter.FlatConfig} */
+module.exports = {
+  linterOptions: {
+    reportUnusedDisableDirectives: true,
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+  },
+};
