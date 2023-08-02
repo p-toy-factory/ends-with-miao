f4f816a75b0ae08544840e18e3e56a4564bc074f
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2023-08-03 00:39:14 +0800

    chore: release v0.2.0

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# ends-with-miao
+# 以“喵”字作为句子结尾喵 ends-with-miao
 
-使用生成式人工智能技术，将网页中的每一个句子结尾都加上「喵」字作为结尾的[篡改猴](https://www.tampermonkey.net/?locale=zh)脚本喵。
+~~使用生成式人工智能技术，~~ 将网页中的每一个句子结尾都加上「喵」字作为结尾的[篡改猴（Tampermonkey）](https://www.tampermonkey.net/?locale=zh)脚本喵。
 
 ## 已知问题
 
-只能在少部分站点中生效喵。
+只能在少部分站点中生效，可以在点击右键菜单或浏览器插件栏的篡改猴（Tampermonkey），找到选项 _以“喵”字作为句子结尾喵_ -> _手动触发_ 喵。
 
 ## 安装
 
-你需要先在你的浏览器上安装篡改猴，才能安装此脚本喵。
+你需要先在你的浏览器上安装篡改猴（Tampermonkey），才能安装此脚本喵。
 
-[Tampermonkey (GitHub)](https://raw.githubusercontent.com/p-toy-factory/ends-with-miao/main/packages/userscript/dist/index.user.js)
+[Install (on GitHub)](https://raw.githubusercontent.com/p-toy-factory/ends-with-miao/main/packages/userscript/dist/index.user.js)
 
 ## 栗子
 
--- packages/userscript/dist/index.user.js
+++ packages/userscript/dist/index.user.js
@@ -2,27 +2,28 @@
 // ==UserScript==
 // @name         以“喵”字作为句子结尾喵
 // @author       pinkchampagne
-// @description  
+// @description  将网页中的每一个句子结尾都加上「喵」字作为结尾的篡改猴脚本喵。
 // @downloadURL  https://raw.githubusercontent.com/p-toy-factory/ends-with-miao/main/packages/userscript/dist/index.user.js
 // @updateURL    https://raw.githubusercontent.com/p-toy-factory/ends-with-miao/main/packages/userscript/dist/index.user.js
-// @grant        none
+// @grant        GM_registerMenuCommand
 // @homepage     https://github.com/p-toy-factory/ends-with-miao#readme
 // @license      AGPL-3.0
 // @match        *://*/*
 // @supportURL   https://github.com/p-toy-factory/ends-with-miao/issues
 // @run-at       document-idle
-// @version      0.1.14514
+// @version      0.2.0
 // ==/UserScript==
 (() => {
   "use strict";
-  requestIdleCallback((() => {
+  function miao() {
     const iterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
-    for (let currentNode = iterator.nextNode(); Boolean(currentNode); currentNode = iterator.nextNode()) {
+    for (let currentNode = iterator.nextNode(); currentNode; currentNode = iterator.nextNode()) {
       const {textContent} = currentNode, textContentEndsWithMiao = function(input) {
-        return /[\u3041-\u30FF]/.test(input);
-      }(input = currentNode.textContent) ? input : input.replace(/(?:(?!喵)([\u4E00-\u9FA5]))([！。…!.])/g, "$1喵$2");
+        return /\p{sc=Hira}|\p{sc=Kana}/gu.test(input);
+      }(input = textContent) ? input : input.replace(/(?:(?!喵)(\p{sc=Han}))([！。…!.])/gu, "$1喵$2");
       textContent !== textContentEndsWithMiao && (currentNode.textContent = textContentEndsWithMiao);
     }
     var input;
-  }));
+  }
+  requestIdleCallback(miao), GM_registerMenuCommand("手动触发", miao);
 })();
